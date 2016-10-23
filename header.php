4189c3971825c9ac97e67d8ed3df6d8d22bb6ffe
Ivonne Karamoy <ikaramoy@gmail.com>
2016-10-23 14:01:09 -0400

    Update: Adding Sara Hicks interview (not complete)

--- header.php
+++ header.php
@@ -6,14 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css">
   <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/28b6dbe0-7189-4bf0-b1ff-a727583465ca.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body class="theme-coral">
-  
+
   <!-- global header -->
   <header class="header-primary">
     <a href="index.html" class="logo">
-      <img src="assets/img/W&&T-full-logo-outline.svg" alt="Women && Tech logo">
+      <img src="/assets/img/W&&T-full-logo-outline.svg" alt="Women && Tech logo">
       <h1 class="screen-readers">Women && Tech</h1>
     </a>
     <nav class="nav-primary">
