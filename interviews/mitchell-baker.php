734e99e55eced2abe2b8184f7df37e1e8f09c066
Cassie Kaiser <kaiser.cassie@gmail.com>
2016-10-23 14:01:17 -0400

    Update: Adding Mitchell Baker's interview - Still in progress - Added some text - Added some images

---
+++ interviews/mitchell-baker.php
@@ -0,0 +1,132 @@
+<?php include '../header.php'; ?>
+  <main role="main">
+
+<!-- Featured Image -->
+    <figure class="border img-feature">
+      <img src="/assets/img/mitchell-baker/featured-mitchell-baker.jpg" alt="Mitchell Baker">
+    </figure>
+
+<!-- Interview Header -->
+    <header class="border header-interview">
+  <div class="wrapper-sm">
+    <h2>Mitchell Baker</h2>
+    <p class="intro">Mitchell Baker is the recipient of numerous awards and recognition, known for her intimidating pauses and sharp intellect. She is the face and founder of the non-profit tech company <a href="https://www.mozilla.org/en-US/?v=a">Mozilla</a>, creator of Firefox and champion of the open web.</p>
+    <p class="author">An interview with <a href="https://twitter.com/cassiemc">Cassie McDaniel</a></p>
+
+    <ul class="social-share">
+      <li>
+        <a href="//facebook.com/WomenAndTech" title="Facebook @WomenAndTech">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <span class="screen-readers">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a href="//twitter.com/WomenAndTech" title="Twitter @WomenAndTech">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <span class="screen-readers">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a href="mailto:contact@womenandtech.com" title="Email contact@womenandtech.com">
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <span class="screen-readers">Email contact@womenandtech.com</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</header>
+
+<!-- Q&A -->
+<dl class="border">
+  <dt class="interview-q">I am curious to know how you got your name?</dt>
+  <dd class="interview-a">
+    <p>Let’s see. Oh God. My name. My father was raised by his stepfather, whose last name was Mitchell, from the time my dad was ten or so. My parents thought it would be a nice way to recognize him, so they would call me Mitchell Baker. Then my mom says at the last minute, “Oh, if it’s a girl, maybe she should have a feminine name.” So my first name, Winifred, is my grandmother’s name, whom I never met nor do I know anything about.</p>
+    <p>My mom was eccentric. She was exceedingly eccentric. Sometimes you know in a way that was really endearing and charming and sometimes in a way I would look at her and think, “Wow, that’s really out there.”</p>
+    <p>Somewhere along the line I was writing a résumé or something and my mother saw a draft of it and said, “I don’t like Winifred as your name and I hope you won’t use it. I’d like you to use the name I intended to give you which is W. Mitchell Baker.” She was pretty determined about it, and so I said okay. But it’s an awkward thing when you’re in college not to have a name or an identity that’s consistent.</p>
+    <p>I find, having a name that’s unusual, that when I meet people if I’m not careful they don’t talk to me directly. English speakers in particular. Mitchell’s a man’s name, and there I am — they really don’t believe it or understand it, it doesn’t sound like Michelle — but because they don’t know my name, they’re uncomfortable talking to me. So I have a bunch of adjustments I’ve learned to do to help make people comfortable with my name.</p>
+    <p>Funny thing — I have a Chinese name too and it is also unusual and requires explanation. It’s been sort of a constant throughout my life.</p>
+  </dd>
+
+  <dt class="interview-q">How did you get your Chinese name?</dt>
+  <dd class="interview-a">
+    <p>From my first Chinese teacher at Berkeley. For my first name, she gave me a choice of a couple of characters. The meaning of one was I think, ‘sweet’, or ‘honey’, ‘very nice’, and another character that meant ‘agile’ or ‘clever’. And so I said, “Oh I want that one.” Am I very sweet and nice, or agile and clever? I’ll take the latter!</p>
+  </dd>
+</dl>
+
+    <figure class="border img-landscape">
+      <img src="/assets/img/mitchell-baker/landscape-mitchell-baker.jpg" alt="Mitchell Baker">
+
+      <figcaption>
+        Photo credit: Inc. Magazine, Forbes Magazine , Tristan Nitot , Jay Patel , and Gen Kanai
+      </figcaption>
+    </figure>
+
+<dl class="border">
+  <dt class="interview-q">Asian Studies was an unusual area of study when you were in school. Is your life what you thought it would turn out to be?</dt>
+  <dd class="interview-a">
+    <p>Well that’s a funny thing where I think I really am different from a lot of people. I was never one to plan, I just don’t think that way. I don’t even think five years ahead. As far as I can tell I don’t actually have role models. I just don’t look at the world that way. Maybe I’d be better off if I did, but who could say.</p>
+    <p>I don’t think I could have imagined my life. It’s been an odd path. I mentioned my mom was eccentric. My dad didn’t follow a model either. So I think I just look at the options in front of me and walk the most interesting path.</p>
+  </dd>
+</dl>
+
+  <aside class="border pull-quote">
+    <p>I just look at the options in front of me and walk the most interesting path.</p>
+  </aside>
+
+<dl class="border">
+  <dt class="interview-q">Can you tell me about your parents and what they did? Why was your mom eccentric?</dt>
+  <dd class="interview-a">
+    <p>I don’t know why my mom was eccentric, there was really nothing in her background that would lead you to think she would be eccentric, except maybe that she was determined to work.</p>
+    <p>Her world was really different. In her era if women did any educational thing after high school it was either secretarial school or what she chose instead; she was a graphic artist. There was a creative sense and hand-eye coordination, because before the computer you would physically put each image on the page and lay out every ad by hand.</p>
+    <p>She had a lot of freedom and flexibility and she got married late in life. Married a man who had been married before, had lots of stuff that wasn’t really normal. And I think she liked a sense of adventure. But I have no idea where or why it came from, it was just her.</p>
+  </dd>
+</dl>
+
+  <figure class="border img-landscape">
+    <img src="/assets/img/mitchell-baker/landscape-mitchell-baker-kitchen.jpg" alt="Mitchell Baker">
+
+    <figcaption>
+      Photo credit: Joi Ito
+    </figcaption>
+  </figure>
+
+<dl class="border">
+  <dd class="interview-a">
+    <p>My dad was a classic old-style entrepreneur, where you scraped together enough money and hoped enough checks and cash came in each week to keep it open. It’s pretty interesting, the old entrepreneurial style of America before venture capital and before computers and technology.</p>
+    <p>He really did not like working for other people. He was a workaholic — he worked all the time, but he needed to be setting his own direction, so he had a series of businesses throughout his life, a few of them that were really successful. Most of them related to metal. He had been trained as a jeweler long ago then got a degree in mechanical engineering, sort of an inventor for awhile.</p>
+    <p>He was an iconoclast and he loved discussions that bordered on argument, loved nothing more than to get a table full of people and make some outrageous statement about politics or this or that and then see what happened. I grew up as an only child and he was aggressive in his verbal sparring. It wasn’t personal at all — I mean, my dad loved me like mad, so it wasn’t a question of worth or value, it was about intellectual exchange. And I think that was phenomenal training.</p>
+  </dd>
+</dl>
+
+  <figure class="border img-landscape">
+    <img src="/assets/img/mitchell-baker/landscape-mitchell-baker-speaking.jpg" alt="Mitchell Baker">
+
+    <figcaption>
+      Photo credit: Hubert Figuière
+    </figcaption>
+  </figure>
+
+<dl class="border">
+  <dt class="interview-q">Do you think your dad influenced your gravitation toward law?</dt>
+  <dd class="interview-a">
+    <p>I don’t know. The law piece was really different. When you get into business and professional settings where people are engaged or aggressive or loud or are really projecting themselves, experience in that setting is incredibly useful. Most of my career there was rarely another woman in the room. We’ve changed that on the Mozilla board now, but for most major decisions there was never another woman.</p>
+    <p>Periodically some people I work with would ask me, “Are you okay?” I’d think, “I’m a partner at Mozilla and, yes I’m fine, that discussion did not bother me.” I can have a heated discussion on the elements and I can have people get frustrated with me and not feel crushed and not feel like it’s the end of the world. And still feel like maybe I overreacted some, but I still think my point of view is right and I’m not going to give it up.</p>
+    <p>So that constant sparring, contradiction, and ‘let’s try to push your buttons so I can see what happens’ — in the setting of a man who I knew adored me – I think that was one of those intangible trainings you can’t put on a résumé but that has been critical to my success.</p>
+  </dd>
+</dl>
+
+  <aside class="border pull-quote">
+    <p>I can have a heated discussion on the elements and I can have people get frustrated with me and not feel crushed and not feel like it’s the end of the world.</p>
+  </aside>
+
+<dl class="border">
+  <dt class="interview-q">Do you think your communication style might be typically considered male?</dt>
+  <dd class="interview-a">
+    <p>Well, first you’d have to ask others what their experience is. Probably a yes.</p>
+    <p>I like to think I have a range of communication styles that I can adjust based on my understanding of how the person I’m talking to responds best. I do feel like I’m a mix between my parents.</p>
+    <p>I don’t think it’s a flat out yes, but I am certainly comfortable in groups of men vigorously debating a topic.</p>
+  </dd>
+</dl>
+
+  </main>
+<?php include '../footer.php'; ?>
