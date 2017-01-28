765b1b514fae1392c2113316ba1b4497b3805d98
Christina Truong <hello@christinatruong.com>
2017-01-28 13:31:49 -0500

    Updating readme instructions and base template file paths.

--- interviews/base-template.php
+++ interviews/base-template.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
   /* Fill out these values to customize the page.
   $page_title = Interviewee's name
   $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
   */
-  
-  $page_title = 'Sarah Hicks';  
+
+  $page_title = 'Sarah Hicks';
   $page_theme = 'theme-coral';
-  include '../header.php'; 
+  include '../../header.php';
 ?>
 
   <main role="main">
 
   </main>
-  
-<?php include '../footer.php'; ?>
+
+<?php include '../../footer.php'; ?>
