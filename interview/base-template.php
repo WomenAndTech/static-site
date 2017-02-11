c615eaef81a3eb4f1466860d696055ba41c827fb
Christina Truong <hello@christinatruong.com>
2017-02-11 13:59:46 -0500

    Recent/past interviews: - created global include file - updated base-template to add include file

--- interview/base-template.php
+++ interview/base-template.php
@@ -4,13 +4,16 @@
   $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
   */
 
-  $page_title = 'Sarah Hicks';
+  $page_title = '';
   $page_theme = 'theme-coral';
   include '../../header.php';
 ?>
 
   <main role="main">
 
+
+    <!-- // Latest 3 Interviews -->
+    <?php include '../../latest-interviews.php'; ?>
   </main>
 
 <?php include '../../footer.php'; ?>
