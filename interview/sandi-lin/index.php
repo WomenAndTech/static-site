5ac2be9bb7396705ee004610f4207e4cfe450efd
Cassie Kaiser <kaiser.cassie@gmail.com>
2017-02-11 12:01:20 -0500

    Updated footer

--- interview/sandi-lin/index.php
+++ interview/sandi-lin/index.php
@@ -355,39 +355,6 @@
      </p>
    </div>
 
-   <!-- // Past Interviews -->
-   <aside class="past-interviews">
-     <div class="wrapper">
-       <h2>Past Interviews</h2>
-       <div class="grid-wrapper">
-         <section class="grid-3-1">
-           <a href="#link-to-interview" class="no-underline">
-             <img src="/assets/img/thumbnail-sara-hicks.png" alt="Sara Hicks">
-             <h3>Sarah Hicks</h3>
-             <p>with Sophie He</p>
-             <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-           </a>
-         </section>
-         <section class="grid-3-1">
-           <a href="#link-to-interview" class="no-underline">
-             <img src="/assets/img/thumbnail-mitchell-baker.png" alt="Mitchell Baker">
-             <h3>Mitchell Baker</h3>
-             <p>with Cassie McDaniel</p>
-             <p>Founder of the non-profit tech company Mozilla, creator of Firefox and champion of the open web.</p>
-           </a>
-         </section>
-         <section class="grid-3-1">
-           <a href="#link-to-interview" class="no-underline">
-             <img src="/assets/img/thumbnail-sandi-lin.png" alt="Sandi Lin">
-             <h3>Sandi Lin</h3>
-             <p>with Sarah Sutherland</p>
-             <p>Co-founder and CEO of Seattle-based startup Skilljar, an online course delivery platform.</p>
-           </a>
-         </section>
-       </div>
-     </div>
-   </aside>
-
   </main>
 
 <?php include '../../footer.php'; ?>
