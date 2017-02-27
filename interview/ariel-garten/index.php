19fac7c6dbadb87dcf1bd58174a10e7cd6aa3dab
Ivonne Karamoy <ikaramoy@gmail.com>
2017-02-26 22:17:56 -0500

    Ariel Garten’s interview

---
+++ interview/ariel-garten/index.php
@@ -0,0 +1,263 @@
+<?php
+  /* Fill out these values to customize the page.
+  $page_title = Interviewee's name
+  $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
+  */
+
+  $page_title = 'Ariel Garten';
+  $page_theme = 'theme-green';
+  include '../../header.php';
+?>
+
+  <main role="main">
+
+    <!-- Featured Image -->
+    <div class="border img-feature">
+      <figure>
+        <img src="featured-ariel-garten.jpg" alt="Ariel Garten">
+      </figure>
+    </div>
+
+    <!-- Interview Header -->
+    <header class="border header-interview">
+      <div class="wrapper-sm">
+        <h2>Ariel Garten</h2>
+        <p class="intro">Ariel is an artist, scientist and entrepreneur who has bridged the gap between all three disciplines. She is the CEO of InteraXon, a company specialized in thought-controlled computing and has raised $287,472 on Indiegogo to produce a <a href="http://www.indiegogo.com/interaxonmuse" target="_blank" title="InteraXon Muse" alt="InteraXon Muse">brain-sensing headband</a>.</p>
+        <p class="author">An interview with <a href="http://twitter.com/milang" rel="author" target="_blank" title="Milan Gokhale's twitter profile"><strong>Milan Gokhale</strong></a></p>
+
+        <ul class="social-share">
+          <li>
+            <a href="//facebook.com/WomenAndTech" title="Facebook @WomenAndTech">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+              <span class="screen-readers">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a href="//twitter.com/WomenAndTech" title="Twitter @WomenAndTech">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+              <span class="screen-readers">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:contact@womenandtech.com" title="Email contact@womenandtech.com">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span class="screen-readers">Email contact@womenandtech.com</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+
+    <!-- Q&A -->
+    <dl class="border">
+      <dt class="interview-q">Please introduce yourself.</dt>
+      <dd class="interview-a">
+        <p>My name is Ariel Garten, I’m the CEO and co-founder of <a href="http://interaxon.ca" target="_blank" title="InteraXon's website" alt="InteraXon">InteraXon</a>. At InteraXon we create thought controlled computing products, applications and experiences. Thought-controlled computing is exactly what it sounds like — it’s the ability to interact with content and yourself. A sensor sits on your forehead, reads your brainwaves, and then lets you play games on your smartphone or tablet directed by your brain. It also lets you see your brain and engagement activity to improve your working memory, concentration, circulation etc.</p>
+      </dd>
+    </dl>
+
+    <!-- Q&A -->
+    <dl class="border">
+      <dt class="interview-q">Tell us more about your background.</dt>
+      <dd class="interview-a">
+        <p>My background spans arts, science and business. I was in real estate since I was a kid. My dad sent me to show the apartments. At university, I studied neuroscience and ran a clothing line that I had started in high school. I sold to stores in Toronto, and I had a job in a research lab. So I’ve always been able to move back and forth between art and science, and I find both completely stimulating. When I graduated, I continued to work at the research lab and opened my own clothing store on College Street, expanding to sell my line to small boutiques across North America.</p>
+        <p>I was looking for a way to talk about the self and understand the self from scientific and artistic perspectives. I started working with brainwaves in <a href="http://n1nlf-1.eecg.toronto.edu/" title="Steve Mann's Personal Web Page" target="_blank">Steve Mann’s lab</a> almost a decade ago. Ultimately we created a system that I took out of the lab and started to commercialize with my two co-founders, Trevor Colemen and Chris Aimone. That was the formation of InteraXon, which has existed for the last five years. <a href="http://www.youtube.com/watch?v=uJS8EJwGx3o" target="_blank" title="InteraXon Bright Ideas Promotional Video on YouTube">Our first major project</a> was the Vancouver 2010 Winter Olympics. As our kick off project, it wasn’t a bad one to do! Since then, we’ve worked with companies like Deutsche Telekom, brands like Wrigley’s and agencies like the <a href="http://news.cnet.com/8301-17938_105-20009785-1.html#ixzz11PCoiU7v" title="Leslie Katz's report on InteraXon's thought-controlled in-flight entertainment systems published on cnet.com" target="_blank">Ontario Aerospace Council</a> to enable this technology across multiple domains.</p>
+      </dd>
+    </dl>
+
+    <!-- // Image - Landscape -->
+    <div class="border img-landscape">
+      <figure>
+        <img src="landscape-ariel-garten-mounties-banff.jpg" alt="Ariel Garten for InteraXon at Vancouver Olympics">
+        <figcaption>
+          Photo credit: <a href="http://www.flickr.com/photos/k-ideas/5835437178/" target="_blank" title="Ariel Garten, CEO interaxon">Kempton Lam</a>
+        </figcaption>
+      </figure>
+    </div>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">How do you define ‘technology’?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>
+            Technology is an enabler. Anything is a technology; the glasses we wear are technology; there is technology in my shoes. The definition we use today is very specific and it tends to refer to digital or computational technologies that enable our lives in ways that weren’t possible two and a half decades ago.
+          </p>
+      </dd>
+    </dl>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">What were your earliest interactions with technology?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>I was never really into technology. It wasn’t something that I identified with. It wasn’t a driving force. But in the creation of this brainwave technology, there was a lot of processing involved to create the core of the experience. Starting work in the early 2000s on early brain wave systems was my first real foray into technology as something that was part of my own identity.</p>
+      </dd>
+    </dl>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">What drew you to science at an early age?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>My interest in science didn’t overlap with my interest in technology, until recently, in my mind. The interest in science — it seems so obvious to me, it’s how the world works! Why wouldn’t you be fascinated with the mechanisms that create everything?</p>
+      </dd>
+    </dl>
+
+    <!-- // Pull Quote -->
+    <aside class="border pull-quote">
+      <p>It seems so obvious to me, [science is] how the world works! Why wouldn’t you be fascinated with the mechanisms that create everything?</p>
+    </aside>
+
+    <!-- // Image - Landscape -->
+    <div class="border img-landscape">
+      <figure>
+        <img src="landscape-ariel-garten-next-media.jpg" alt="Ariel Garten speaking">
+        <figcaption>
+          Photo credit:  <a href="http://www.flickr.com/photos/nextmediaevents/5831442802/" target="_blank" title="nextMEDIA Keynote: Thought Controlled Computing">Marta Blicharz</a>
+        </figcaption>
+      </figure>
+    </div>
+
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">Did your family encourage you to pursue science?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>There were no family or peers influencing me to go into science. <a href="http://www.vreiss.com/" title="Vivian Reiss' portfolio website">My mother is an artist</a>; my father is in real estate and construction. My mother is not connected to science or technology; she’s extraordinarily intelligent and inquisitive but she processes the world through a brush and through linguistics. And my dad isn’t scientific. He would still have a VCR if someone didn’t throw it out! The pursuit of science was just a line of inquiry to understand how the world works.</p>
+      </dd>
+    </dl>
+
+    <dl class="border">
+      <dt class="interview-q">At what point did science became an important influence in your life?</dt>
+      <dd class="interview-a">
+        <p>As a kid, I hated computers. I thought they were anti-natural, and I was very into the natural world. Science is a part of the natural world. In high school I did great at science and I did okay at math, but I was into abstract concepts. I loved logic — loved, loved, loved logic, like I loved doing puzzles. When everyone in university hated multiple choice, I loved multiple choice. Just to know the totality of the answers!</p>
+      </dd>
+    </dl>
+
+    <!-- // Pull Quote -->
+    <aside class="border pull-quote">
+      <p>I did great at science and I did okay at math, but I was into abstract concepts. I loved logic — loved, loved, loved logic.</p>
+    </aside>
+
+    <dl class="border">
+      <dt class="interview-q">Why do you think there aren’t more young women choosing to pursue a career in science?</dt>
+      <dd class="interview-a">
+        <p>There’s definitely a tendency where people can, from a young age, identify, ‘I’m good at this, I’m not good at this, and this is scary.’ And if you decide you aren’t good at something, you don’t take the time to build the skill. You shut down and you don’t engage with it.</p>
+        <p>I’m also a psychotherapist. I had a patient recently who said, “Numbers scare me. You say the word finance and I want to jump under the table over there.” And it was a process of moving aside her fear, seeing these things as neutral, allowing herself to open up to numbers so that she could spend time learning without feeling anxiety and insecurity.</p>
+        <p>Insecurity is definitely an issue when you are growing up. And when you do something that you don’t know the answer to, your response to not knowing will determine your ability to learn it.</p>
+        <p>If your response to not knowing is, ‘I don’t know it, therefore I feel uncomfortable, therefore I want to run away because it makes me feel insecure,’ then you’re not going to have the confidence, patience, willingness and openness to sit down and say, ‘I don’t know this but I'm not scared, I'm just going to sit here and figure it out.’ Your early responses and experiences will determine whether you go into a subject or not.</p>
+        <p><em>[Editor's Note: Ariel took the stage at Toronto’s 2011 TEDx conference <a href="http://www.tedxtoronto.com/talks/redefining-consciousness-ariel-garten-ceo-and-co-founder-interaxon/" title="Ariel's TED talk on Redefining Consciousness" target="_blank">to give a speech</a> on how thought-controlled computing may work to help us understand the self. It's Ariel's fascination for knowing the self which led her to submerge herself in art, study neuroscience and later become a psychotherapist.]</em></p>
+      </dd>
+    </dl>
+
+    <!-- // Image - Landscape -->
+    <div class="border img-landscape">
+      <figure>
+        <img src="landscape-ariel-garten-steve-wozniak.jpg" alt="Ariel Garten with Steve Wozniak">
+        <figcaption>
+          Photo credit: <a href="http://www.anglemediagroup.com/interaxon-paving-the-way-with-your-brain/" title="Company blog post containing video: InteraXon: Paving the Way with Your Brain" target="_blank">Angle Media Group</a>
+        </figcaption>
+      </figure>
+    </div>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">Why do you think there is an imbalance between the number of men and women working in technology?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>I'm tempted to think that early on there was a misidentification about who was good at math and who wasn't. And as we break down gender barriers, my guess would be that in classrooms now you see much less differentiation between the number of males and females. There are so many girls who rock at math!</p>
+        <p>In my generation, to get into computational neuroscience you had to do computer science first. I think in 15 years, in fields like computational neuroscience where there are equal opportunities to learn computer science, or if it becomes acceptable for girls to sit in their basement and figure out code, we’ll see leveling off of males vs. females.</p>
+      </dd>
+    </dl>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">Can you tell us about your role as a female tech entrepreneur?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>Well, I'm definitely in the vast minority. For example, I was invited to <a href="http://techcrunch.com/2012/05/28/f50/" title="Alexia Tsotsis' article on the Founders Fund's first attempt at a tech conference published on TechCrunch.com">a tech conference for the 50 brightest minds in technology</a>. They flew us on a private jet to Hawaii, made it a big deal, it was pretty cool. There were 50 people — only five were women.</p>
+        <p>As a rare bird in this full nest, I have to say I don’t mind. I can work being female to my advantage. We joke that if our demo isn’t working at a conference, I can just wear the headset and smile and that would be enough.</p>
+        <p>Everyone has their own advantages regardless of gender. I’m in a unique position at this point in time as a woman tech CEO.</p>
+      </dd>
+    </dl>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">How do you think it is advantageous to be a woman in technology?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>I'm aware that because I am a rare female in the tech executive space, I may receive special treatment. And probably part of my success is due to the fact that I’m female. It’s fetishized, not in a sexual way, but through that fetishization the female role becomes iconic, creating an archetype other people can follow. That leads to role models and creates a path for more women.</p>
+      </dd>
+    </dl>
+
+    <!-- // Image - Landscape -->
+    <div class="border img-landscape">
+      <figure>
+        <img src="landscape-ariel-garten-web2012.jpg" alt="Ariel Garten at LeWeb 2012">
+        <figcaption>
+          Ariel has spoken at LeWeb 2012 where she demoed Muse. <em>Photo credit: <a href="https://plus.google.com/u/1/photos/100261394308614575193/albums/5818092704713995425/5818092713913684722" target="_blank">Caroline Bindner</em></a>
+        </figcaption>
+      </figure>
+    </div>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">Will we reach a more equal balance between male and female CEOs in the next 50 years?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>I don't think there should be any natural order, unless not being that way leads to some sort of system of oppression. And I guess a feminist would argue that this kind of inequality is inherently systemic and self-perpetuating oppression. [Pauses] I can buy into that, but it's not naturally how I feel. I've never felt oppressed as a female. I've never felt that I have been withheld opportunity because of my gender. I have not been inspired to the deep feeling of belief that comes with those feminist feelings.</p>
+      </dd>
+    </dl>
+
+    <!-- // Pull Quote -->
+    <aside class="border pull-quote">
+      <p>I've never felt oppressed as a female. I've never felt that I have been withheld opportunity because of my gender.</p>
+    </aside>
+
+    <dl class="border">
+      <dd class="interview-a">
+        <p>To me, everything is fine and normal. I definitely recognize that there are other people that don't have that experience. It's unfair of me to think that because everything is fine for me, everything is fine. But realistically, it feels normal to me.</p>
+      </dd>
+    </dl>
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">What are your thoughts on feminist movements for women in technology?</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>There are two schools of thought. One is a school that says, ‘Yes, we remember that there's a problem and we have to beat the drum in order to solve the problem.’ The other says, ‘It’s not really much of a problem, and if we don’t make it a problem, you can do anything you put your mind to.’ And depending on your level of feminism, you could fall into either school.</p>
+      </dd>
+    </dl>
+
+
+    <!-- // Q & A -->
+    <dl class="border">
+      <!-- // Question -->
+      <dt class="interview-q">In our last interview <a href="/interview/lucia-mariani-vena/">Lucia Mariani-Vena</a> asked the question “When will we see the female version of Steve Jobs or Mark Zuckerberg?”</dt>
+      <!-- // Answer -->
+      <dd class="interview-a">
+        <p>My cocky, ‘masculine’ answer would be me in five years [laughs]. I have no fucking idea! But my first thought was, ‘that could be me!’</p>
+      </dd>
+    </dl>
+
+    <!-- // Footer Credits -->
+    <footer class="border credits">
+      <p>This site uses Monotype's Avenir Next by Adrian Fruteger and Akira Kobayashi, Github, Cloudflare, and Windows Azure.</p>
+    </footer>
+
+    <!-- // Latest 3 Interviews -->
+    <?php include '../../latest-interviews.php'; ?>
+
+  </main>
+<?php include '../../footer.php'; ?>
