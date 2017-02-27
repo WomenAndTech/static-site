7b124b3b45995bcd991e9189bbacdfdb8fad1ed5
Ivonne Karamoy <ikaramoy@gmail.com>
2017-02-26 22:58:20 -0500

    Infographic page

---
+++ infographic-paid-maternity-leave/index.php
@@ -0,0 +1,36 @@
+<?php
+  /* Fill out these values to customize the page.
+  $page_title = Interviewee's name
+  $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
+  */
+
+  $page_title = 'Infographic: Parental Leave Worldwide';
+  $page_theme = 'theme-green';
+  include '../header.php';
+?>
+
+<main role="main">
+
+  <!-- // Intro -->
+  <article class="border">
+    <header class="border header-interview">
+    <div class="wrapper-sm">
+      <p class="intro">In <a title="Lucia Mariani-Vena Interview" href="/interview/lucia-mariani-vena/">our first interview</a>, Lucia Mariani-Vena shared some poignant insights about what it was like to be a working mother. She told us “you have to feel like you have the support to do what you need to do,” so we wanted to investigate the systems that support parents. How do different countries help mothers (and fathers) during early parenthood?</p>
+    </div>
+    </header>
+  </article>
+
+  <!-- // Image - Portrait -->
+  <div class="border img-portrait">
+    <figure>
+      <img src="WomenAndTech_MaternityLeaveInfographic.png" alt="Infographic: Worldwide Parental Leave">
+    </figure>
+  </div>
+
+  <!-- // Footer Credits -->
+  <footer class="border credits">
+    <p>This site uses Monotype's Avenir Next by Adrian Fruteger and Akira Kobayashi, Github, Cloudflare, and Windows Azure.</p>
+  </footer>
+
+</main>
+<?php include '../footer.php'; ?>
