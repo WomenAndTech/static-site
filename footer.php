6391072f24f3194999f0a7d248bed331a9be52a7
Christina Truong <hello@christinatruong.com>
2017-03-03 17:55:26 -0500

    Renaming directories to match current URLs.

--- footer.php
+++ footer.php
@@ -29,8 +29,6 @@
   <script>!window.jQuery && document.write('<script src="assets/js/jquery-2.2.3.min.js"><\/script>')</script>
   <!-- Fonts -->
   <script type="text/javascript" src="//fast.fonts.net/jsapi/28b6dbe0-7189-4bf0-b1ff-a727583465ca.js"></script>
-  <!-- Syntax highlighter -->
-  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>
   <script src="assets/js/global-min.js"></script>
 
   <!-- Initialize syntax highlighter -->
