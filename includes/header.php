3500f0bba05ed7c93494149ee948bac102eed069
Christina Truong <hello@christinatruong.com>
2017-03-04 15:49:02 -0500

    Creating includes folder.

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    <?php
+      if ($page_title):
+        echo $page_title . " | Women&amp;&amp;Tech";
+      else:
+        echo "Women&amp;&amp;Tech";
+      endif;
+    ?>
+  </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/28b6dbe0-7189-4bf0-b1ff-a727583465ca.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body class="<?php echo $page_theme; ?>">
+
+  <!-- global header -->
+  <header class="header-primary">
+    <a href="/" class="logo">
+      <img src="/assets/img/W&&T-full-logo-outline.svg" alt="Women && Tech logo">
+      <h1 class="screen-readers">Women && Tech</h1>
+    </a>
+    <nav class="nav-primary">
+      <a href="/archive/" <?php if ($page == "interviews"): echo 'class="active"'; endif; ?>>Interviews</a>
+      <a href="/about/" <?php if ($page == "about"): echo 'class="active"'; endif; ?>>About Us</a>
+      <a href="/contributor-guidelines/" <?php if ($page == "contribute"): echo 'class="active"'; endif; ?>>Contribute</a>
+      <a href="/nominate/" <?php if ($page == "nominate"): echo 'class="active"'; endif; ?>>Nominate</a>
+    </nav>
+  </header>
---
+++ includes/site-credits.php
@@ -0,0 +1,1 @@
+<p>This site uses Monotype's Avenir Next by Adrian Fruteger and Akira Kobayashi, Github, Cloudflare, and Windows Azure.</p>
