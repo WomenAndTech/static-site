7dda9a04d72aebd543b86df8d9a7e546061e4405
Christina Truong <hello@christinatruong.com>
2017-03-06 11:30:41 -0500

    Updating base-template and index page instructions.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
   // Update $current to the folder name of interviewee to display on homepage.
-  $current = "ariel-garten";
+  $current = "ayla-newhouse";
+
+  // Do not change these settings
   $homepage = "true";
   include_once 'interview/'.$current.'/index.php';
 ?>
--- interview/base-template.php
+++ interview/base-template.php
@@ -1,11 +1,17 @@
 <?php
   /*
-  Fill out these values to customize the page.
-  $page_title = Interviewee's name
-  $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
+    Fill out these values to customize the page.
+    $page_title = Interviewee's name
+    $page_theme values: theme-coral, theme-purple, theme-yellow, theme-green, theme-blue
+
+    Add this PHP snippet to every image src, followed by the image file name.
+    All images are added to the 'assets > img' directory.
+    <img src="<?php echo $path_img; ?>featured-sandi-lin.jpg" alt="Sandi Lin">
   */
   $page_title = 'Firstname Lastname';
   $page_theme = 'theme-coral';
+
+  /* Do not change these settings */
   include $_SERVER['DOCUMENT_ROOT'].'/config.php';
   include ($path_inc."header.php");
 ?>
