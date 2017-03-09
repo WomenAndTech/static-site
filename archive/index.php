de1adc3a8316e0df14f263c773abf59426c4e5f3
Christina Truong <hello@christinatruong.com>
2017-03-09 17:56:50 -0500

    Updated archive page & latest interviews include.

--- archive/index.php
+++ archive/index.php
@@ -1,165 +1,166 @@
-
 <?php
-  $page = 'interviews';
+  $page = 'archive';
   $page_title = 'Interviews Archive';
   $page_theme = 'theme-coral';
   include $_SERVER['DOCUMENT_ROOT'].'/config.php';
   include ($path_inc."header.php");
 ?>
-  <main role="main">
-    <aside class="past-interviews">
-      <div class="wrapper">
-        <h2>Interviews</h2>
 
-        <div class="grid-wrapper">
-          <section class="grid-3-1">
-            <a href="/interview/sara-hicks/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-sara-hicks.png" alt="Sara Hicks">
-              <h3>Sara Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/mitchell-baker/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-mitchell-baker.png" alt="Mitchell Baker">
-              <h3>Mitchell Baker</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/sandi-lin/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-sandi-lin.png" alt="Sandi Lin">
-              <h3>Sandi Lin</h3>
-              <p>with Sarah Sutherland</p>
-              <p>Co-founder and CEO of Seattle-based startup Skilljar, an online course delivery platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/biying-miao/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-biying-miao.jpg" alt="Biying-miao">
-              <h3>Biying Miao</h3>
-              <p>with Fname Lname</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/nyla-ahmad/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-nyla-ahmad.png" alt="Nyla Ahmad">
-              <h3>Nyla Ahmad</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/gina-luciano/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-gina-luciano.png" alt="Gina Luciano">
-              <h3>Gina Luciano</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/karen-schulman-dupuis/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-karen-schulman-dupuis.png" alt="Karen Schulman Dupuis">
-              <h3>Karen Schulman Dupuis</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/melissa-dominguez/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-melissa-dominguez.png" alt="Melissa Dominguez">
-              <h3>Melissa Dominguez</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/amber-macarthurs/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-amber-macarthurs.png" alt="Amber MacArthur">
-              <h3>Amber MacArthur</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-s.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-          <section class="grid-3-1">
-            <a href="/interview/" class="no-underline">
-              <img src="<?php echo $path_img; ?>thumbnail-.png" alt="Sara Hicks">
-              <h3>Sarah Hicks</h3>
-              <p>with Sophie He</p>
-              <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
-            </a>
-          </section>
-        </div>
-      </div>
-    </aside>
-
-  </main>
+<main role="main" class="past-interviews wrapper-lg">
+  <div class="grid-wrapper">
+    <article class="grid-4-2-1">
+      <a href="/interview/sara-hicks/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-sara-hicks.png" alt="Sara Hicks">
+        <h3>Sara Hicks</h3>
+        <p>with Sophie He</p>
+        <p>Cofounder and CEO of Reaction Commerce, an open-source ecommerce platform.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/mitchell-baker/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-mitchell-baker.png" alt="Mitchell Baker">
+        <h3>Mitchell Baker</h3>
+        <p>with Cassie McDaniel</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/sandi-lin/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-sandi-lin.png" alt="Sandi Lin">
+        <h3>Sandi Lin</h3>
+        <p>with Sarah Sutherland</p>
+        <p>Co-founder and CEO of Seattle-based startup Skilljar, an online course delivery platform.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/biying-miao/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-biying-miao.jpg" alt="Biying Miao">
+        <h3>Biying Miao</h3>
+        <p>with Ruby Watson</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/nyla-ahmad/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-nyla-ahmad.png" alt="Nyla Ahmad">
+        <h3>Nyla Ahmad</h3>
+        <p>with Audrey Kuo</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/gina-luciano/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-gina-luciano.png" alt="Gina Luciano">
+        <h3>Gina Luciano</h3>
+        <p>with Magdalena Georgieva</p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/karen-schulman-dupuis/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-karen-schulman-dupuis.png" alt="Karen Schulman Dupuis">
+        <h3>Karen Schulman Dupuis</h3>
+        <p>with Kelly Seagram</p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/melissa-dominguez/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-melissa-dominguez.png" alt="Melissa Dominguez">
+        <h3>Melissa Dominguez</h3>
+        <p>with Kelly Seagram</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/amber-macarthur/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-amber-macarthur.png" alt="Amber MacArthur">
+        <h3>Amber MacArthur</h3>
+        <p>with Kelly Seagram</p>
+        <p>Amber writes for Fast Company, speaks regularly, and is an entrepreneur several times over.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/pearl-chen/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-pearl-chen.png" alt="Pearl Chen">
+        <h3>Pearl Chen</h3>
+        <p>with Ivonne Karamoy</p>
+        <p>Pearl is a seasoned developer, writer, speaker, and runs tech workshops through her studio, Karma Laboratory.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/martha-ladly/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-martha-ladly.png" alt="Martha Ladly">
+        <h3>Martha Ladly</h3>
+        <p>with Mark Staplehurst</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/dessy-daskalov/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-dessy-daskalov.png" alt="Dessy Daskalov">
+        <h3>Dessy Daskalov</h3>
+        <p>with Lucia Mariani-Vena</p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/nora-young/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-nora-young.png" alt="Nora Young">
+        <h3>Nora Young</h3>
+        <p>with Cassie McDaniel</p>
+        <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/christina-truong/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-christina-truong.png" alt="Christina Truong">
+        <h3>Christina Truong</h3>
+        <p>with Brenna O'Brien</p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/ayla-newhouse/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-ayla-newhouse.png" alt="Ayla Newhouse">
+        <h3>Ayla Newhouse</h3>
+        <p>with Brett Bergmann</p>
+        <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/heather-payne/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-heather-payne.png" alt="Heather Payne">
+        <h3>Heather Payne</h3>
+        <p>with Cassie McDaniel</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/ariel-garten/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-ariel-garten.png" alt="Ariel Garten">
+        <h3>Ariel Garten</h3>
+        <p>with Sophie He</p>
+        <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/infographic-paid-maternity-leave/" class="no-underline">
+        <img src="<?php echo $path_img; ?>infographic-paid-maternity-leave.png" alt="Infographic: Maternity Leave Worldwide">
+        <h3>Infographic: Maternity Leave Worldwide</h3>
+        <p>by Ivonne Karamoy</p>
+        <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim.</p>
+      </a>
+    </article>
+    <article class="grid-4-2-1">
+      <a href="/interview/lucia-mariani-vena/" class="no-underline">
+        <img src="<?php echo $path_img; ?>thumbnail-lucia-mariani-vena.png" alt="Lucia Mariani-Vena">
+        <h3>Lucia Mariani-Vena</h3>
+        <p>with Ivonne Karamoy</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </a>
+    </article>
+  </div>
+</main>
 
 <?php include($path_inc."footer.php"); ?>
--- includes/latest-interviews.php
+++ includes/latest-interviews.php
@@ -5,34 +5,34 @@ Use the folder name of the interviewee for the href.
 */
 
 ?>
-<aside class="past-interviews">
-  <div class="wrapper">
-    <h2>Past Interviews</h2>
+
+  <aside class="past-interviews wrapper">
+    <h2>Latest Interviews</h2>
+    <a class="view-all" href="/archive/">view all</a>
     <div class="grid-wrapper">
-      <section class="grid-3-1">
+      <article class="grid-3-1">
         <a href="/interview/sara-hicks/" class="no-underline">
           <img src="<?php echo $path_img; ?>thumbnail-sara-hicks.png" alt="Sara Hicks">
-          <h3>Sarah Hicks</h3>
+          <h3>Sara Hicks</h3>
           <p>with Sophie He</p>
-          <p>Co-founder and CEO of Reaction Commerce, an open source eCommerce platform.</p>
+          <p>Cofounder and CEO of Reaction Commerce, an open-source ecommerce platform.</p>
         </a>
-      </section>
-      <section class="grid-3-1">
-        <a href="/interview/biying-miao/" class="no-underline">
-          <img src="<?php echo $path_img; ?>thumbnail-biying-miao.jpg" alt="Biying-miao">
-          <h3>Biying Miao</h3>
-          <p>with Fname Lname</p>
+      </article>
+      <article class="grid-3-1">
+        <a href="/interview/mitchell-baker/" class="no-underline">
+          <img src="<?php echo $path_img; ?>thumbnail-mitchell-baker.png" alt="Mitchell Baker">
+          <h3>Mitchell Baker</h3>
+          <p>with Cassie McDaniel</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </a>
-      </section>
-      <section class="grid-3-1">
+      </article>
+      <article class="grid-3-1">
         <a href="/interview/sandi-lin/" class="no-underline">
           <img src="<?php echo $path_img; ?>thumbnail-sandi-lin.png" alt="Sandi Lin">
           <h3>Sandi Lin</h3>
           <p>with Sarah Sutherland</p>
           <p>Co-founder and CEO of Seattle-based startup Skilljar, an online course delivery platform.</p>
         </a>
-      </section>
+      </article>
     </div>
-  </div>
-</aside>
+  </aside>
