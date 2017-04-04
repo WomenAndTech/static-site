4dec7bd4ef59b50f6fa6d20c3ac4af20075ed873
Christina Truong <hello@christinatruong.com>
2017-04-04 12:11:38 -0400

    Added Google Analytics.

--- includes/header.php
+++ includes/header.php
@@ -56,20 +56,11 @@
       }
     }
   </style>
-
-  <!-- Google Tracking -->
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-96774134-1', 'auto');
-    ga('send', 'pageview');
-
-  </script>
 </head>
 <body class="<?php echo $page_theme; ?>">
+
+  <?php //https://support.google.com/analytics/answer/1008080
+  include_once($path_inc."analyticstracking.php") ?>
 
   <!-- global header -->
   <header class="header-primary">
