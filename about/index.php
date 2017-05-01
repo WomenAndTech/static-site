f7069b338e9ea9702acaaaaea26fcdcf130d4ad4
Christina Truong <hello@christinatruong.com>
2017-05-01 14:29:50 -0400

    Revised image links for social sharing. Moved CSS in header to global CSS file.

--- about/index.php
+++ about/index.php
@@ -101,12 +101,12 @@
 
   <footer class="no-border footer-secondary">
     <h2>Contact Us</h2>
-    <p>Follow us on <a class="white-links" href="http://twitter.com/WomenAndTech">Twitter</a>, <a href="http://www.facebook.com/womenandtech/">Facebook</a> and <a href="http://www.instagram.com/WomenAndTech/">Instagram</a>.</p>
+    <p>Follow us on <a href="http://twitter.com/WomenAndTech">Twitter</a>, <a href="http://www.facebook.com/womenandtech/">Facebook</a> and <a href="http://www.instagram.com/WomenAndTech/">Instagram</a>.</p>
     <p>Don't hesitate to <a href="mailto:contact@womenandtech.com">email us</a>.</p>
 
     <h2 class="spacer30">Want to help?</h2>
 
-    <p><a class="no-line" href="https://www.patreon.com/WomenAndTech"><img class="spacer30-bottom no-line" src="<?php echo $path_img; ?>coffee-icon.png" alt="Coffee" width="50px"></a></p>
+    <p><a class="no-underline" href="https://www.patreon.com/WomenAndTech"><img class="spacer30-bottom" src="<?php echo $path_img; ?>coffee-icon.png" alt="Coffee" width="50px"></a></p>
 
     <p><a href="https://www.patreon.com/WomenAndTech">Donate</a> a cup of coffee or tea and get first dibs on new content.</p>
     <p>We also welcome contributors, volunteers and sponsors.</p>
--- includes/header.php
+++ includes/header.php
@@ -21,41 +21,17 @@
   <meta name="twitter:creator" content="@WomenAndTech">
   <meta name="twitter:title" content="<?php echo $social_title; ?>">
   <meta name="twitter:description" content="<?php echo $social_desc; ?>">
-  <meta name="twitter:image" content="<?php echo $path_img;?>thumbnail-<?php echo $interviewee_url; ?>.jpg">
+  <meta name="twitter:image" content="//womenandtech.com<?php echo $path_img;?>thumbnail-<?php echo $interviewee_url; ?>.jpg">
 
   <!-- Facebook Open Graph -->
   <meta property="og:url" content="//womenandtech.com/interview/<?php echo $interviewee_url;?>/">
   <meta property="og:type" content="article">
   <meta property="og:title" content="<?php echo $social_title; ?>">
   <meta property="og:description" content="<?php echo $social_desc; ?>">
-  <meta property="og:image" content="<?php echo $path_img;?>thumbnail-<?php echo $interviewee_url; ?>.jpg">
+  <meta property="og:image" content="//womenandtech.com<?php echo $path_img;?>thumbnail-<?php echo $interviewee_url; ?>.jpg">
 
   <link rel="shortcut icon" href="<?php echo $path_img; ?>favicon-32x32.ico" type="image/x-icon">
   <link rel="stylesheet" href="<?php echo $path_css; ?>style.css">
-  <style>
-
-    .theme-purple .team-listing .name, .theme-coral .team-listing .name, .theme-yellow .team-listing .name ,
-    .theme-blue .team-listing .name, .theme-green .team-listing .name {
-      background:none;
-    }
-
-    .theme-purple .no-border a.no-line, .theme-coral .no-border a.no-line, .theme-yellow .no-border a.no-line,
-    .theme-blue .no-border a.no-line, .theme-green .no-border a.no-line{
-      background:none;
-    }
-
-    .fade-in {
-      -webkit-animation-duration: 0.2s;
-      animation-duration:0.2s;
-    }
-
-
-    @media only screen and (max-width: 660px) {
-      article h2 {
-          margin: 42px 0 22px;
-      }
-    }
-  </style>
 </head>
 <body class="<?php echo $page_theme; ?>">
 
