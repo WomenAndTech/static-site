4db95951d65abf41405d6e4e08aa60ed2771710a
Ray Kao <ray@peopleandcode.com>
2017-05-08 10:21:32 -0400

    Updated deploy to get secret from apache conf file ("environment variable") instead of hard coding.

--- deploy.php
+++ deploy.php
@@ -3,7 +3,7 @@
 $headers = getallheaders();
 $authentication_token = $headers["X-Authentication-Token"];
 $hub_signature = $headers["X-Hub-Signature"];
-$secret = "8fc102f064e415e22a2945a3d420d2fd51ae2a0b"; # Test secret.  Remove and set to environment variable
+$secret = getenv("github_secret"); # Test secret.  Remove and set to environment variable
 $body = file_get_contents('php://input');
 
 $signature = "sha1=" . hash_hmac('sha1', $body, $secret);
