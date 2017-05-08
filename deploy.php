62db3c0217fe3d97cea5359375091b0854c787ed
Ray Kao <ray@peopleandcode.com>
2017-05-07 20:52:09 -0400

    adding in commands to run

--- deploy.php
+++ deploy.php
@@ -8,8 +8,31 @@ $body = file_get_contents('php://input');
 
 $signature = "sha1=" . hash_hmac('sha1', $body, $secret);
 
+$commands = array(
+		'echo $PWD',
+		'whoami',
+);
+
 if(hash_equals($signature, $hub_signature)){
-    
+    foreach($commands as $command) {
+        // Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";      
+    }
 }
+
+echo $output;
 ?>
+
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+
 
