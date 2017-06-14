0c0fac0e716ebee52bc0e3872d09909e94a12110
Ivonne Karamoy <ikaramoy@gmail.com>
2017-06-14 01:08:55 -0400

    Updated latest interview tiles to have Emilie's interview (with Aashni and Sara's). Also updated the home page to show Emilie's interview.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   // Update $current to the folder name of interviewee to display on homepage.
-  $current = "aashni-shah";
+  $current = "emilie-baltz";
 
   // Do not change these settings
   $homepage = "true";
