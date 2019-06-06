e8b829572d3829d5cb16f2600d83844cdf45d4d9
Ivonne Karamoy <ikaramoy@gmail.com>
2019-06-06 00:44:52 -0400

    final edits to closing letter, changes to about page since closing, removed subscription and email link in footer and contribution page in header

--- closing/index.php
+++ closing/index.php
@@ -13,19 +13,19 @@
     <!-- // Secondary Header -->
     <header class="no-border header-secondary">
       <h2>Women&&Tech comes to an end.</h2>
-      <p>Thank you to all of our readers for supporting us through the years. We hope you continue to share your story and the stories of the people who help shape our industry.
+      <p>After 7 years, we've decided to close out our project.<br>A BIG thank you to all of our readers and our community for supporting us through the years! We hope you continue to share your story and the stories of the people who help shape our industry.
       </p>
-      <p><a href="http://womenandtech.com/archive/">View interview archive</a></p>
+      <h4><a href="http://womenandtech.com/archive/">View interview archive</a></h4>
     </header>
 
     <section class="border page-content">
       <div class="wrapper-sm">
         <h2></h2>
-        <p class="type-xsm italic">June 1, 2019</p>
-        <p>We launched this little passion project back in 2012 with a team of six: founder Cassie McDaniel, Ivonne Karamoy, Mark Staplehurst, Brett Bergman, Milan Gokhale and Ray Kao. At the time there were few diversity and inclusion initiatives, and conversations around equal pay were not commonly discussed publicly. We wanted to do something about the lack of female voices in our industry so we set out to highlight the women working in tech, to get to know them, their story and their experiences working in the industry. We hoped that by doing so we would encourage other women and girls to celebrate their own stories, to highlight their intelligence, work and unique experiences.</p>
-        <p>Women&&Tech was volunteer based from the start. While we sought sponsorship to help pay for the project costs, there was never any intention to monetize Women&&Tech. We wanted to maintain the passion, the integrity and the authenticity of the project. In 2017, we redesigned the website and became more intentional about representing not only women's stories but different stories from a more diverse group of women. Over the years we’ve shared stories of amazing women doing incredible things in tech. Women just starting out in their careers as well as those who’ve reached the top level in our industry. We heard from women with different paths, experiences and perspectives. Today, these stories are much easier to find. Look to Medium or your favorite podcast to find them. The interview format has been done many times over and people are sharing their own stories. Conversations and initiatives around diversity, inclusion, and intersectionality have become more commonplace. We know there is still a long way to go but the time has come for our project to end. It’s time to make space for other projects, other voices and to discover what else we can do. We’re proud of the work we’ve done and have seen our mandate come to fruition: To amplify stories of women working in tech. </p>
+        <p class="type-xsm italic">June 6, 2019</p>
+        <p>We launched this little passion project back in 2012 with a team of six: founder <a href="https://twitter.com/cassiemc">Cassie McDaniel</a>, <a href="https://twitter.com/ivonnekn">Ivonne Karamoy</a>, <a href="https://twitter.com/britburger">Mark Staplehurst</a>, <a href="https://twitter.com/brttb">Brett Bergman</a>, <a href="https://twitter.com/milang">Milan Gokhale</a> and <a href="https://twitter.com/raykao">Ray Kao</a>. At the time there were few diversity and inclusion initiatives, and conversations around equal pay were not commonly discussed publicly. We wanted to do something about the lack of female voices in our industry so we set out to highlight the women working in tech, to get to know them, their story and their experiences working in the industry. We hoped that by doing so we would encourage other women and girls to celebrate their own stories, to highlight their intelligence, work and unique experiences.</p>
+        <p>Women&&Tech was volunteer based from the start. While we sought sponsorship to help pay for the project costs, there was never any intention to monetize Women&&Tech. We wanted to maintain the passion, the integrity and the authenticity of the project. In 2017, we redesigned the website and became more intentional about representing not only women's stories but different experiences from a more diverse group of women. Over the years we’ve heard from so many amazing women doing incredible things in tech. Women just starting out in their careers as well as those who’ve reached the top level in our industry. We heard from women with different paths, experiences and perspectives. Today, these stories are much easier to find. Look to Medium or your favorite podcast to find them. The interview format has been done many times over and people are sharing their own stories. Conversations and initiatives around diversity, inclusion, and intersectionality have become more commonplace. We know there is still a long way to go but the time has come for our project to end. It’s time to make space for other projects, other voices and to discover what else we can do. We’re proud of the work we’ve done and have seen our mandate come to fruition: To amplify stories of women working in tech. </p>
 
-        <p>Women&&Tech project would not have been possible without our team and the community at large. We’d like to thank both the tech and the Toronto community for supporting us through the years. </p>
+        <p>The Women&&Tech project would not have been possible without our team and the community at large. We’d like to thank both the tech and the Toronto community for supporting us through the years. </p>
 
         <h2>Special thanks to...</h2>
         <h3>Our friends and sponsors:</h3>
@@ -41,15 +41,15 @@
         <aside class="team-listing" style="padding-bottom: 20px; padding-top: 10px;">
           <ul style="margin-bottom: 20px;">
             <li>
-              <p class="name"><a href="http://twitter.com/CassKaiser">Cassie McDaniel</a></p>
+              <p class="name"><a href="https://twitter.com/cassiemc">Cassie McDaniel</a></p>
               <p class="role">Founder, Director, Designer & Editor-in-Chief</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/raykao">Mark Staplehurst</a></p>
+              <p class="name"><a href="http://twitter.com/britburger">Mark Staplehurst</a></p>
               <p class="role">Art Director & Developer</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/raykao">Ivonne Karamoy</a></p>
+              <p class="name"><a href="http://twitter.com/ivonnekn">Ivonne Karamoy</a></p>
               <p class="role">Designer & Managing Director</p>
             </li>
             <li>
@@ -57,23 +57,23 @@
               <p class="role">Developer & Back-End Support</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/raykao">Milan Gokhale</a></p>
+              <p class="name"><a href="http://twitter.com/milang">Milan Gokhale</a></p>
               <p class="role">Social Media Engagement</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/ivonnekn">Brett Bergman</a></p>
+              <p class="name"><a href="http://twitter.com/brttb">Brett Bergman</a></p>
               <p class="role">Developer</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/ivonnekn">Cassie Kaiser</a></p>
+              <p class="name"><a href="http://twitter.com/CassKaiser">Cassie Kaiser</a></p>
               <p class="role">Volunteer Coordinator & Designer</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/ivonnekn">Brenna O'Brien</a></p>
+              <p class="name"><a href="http://twitter.com/brnnbrn">Brenna O'Brien</a></p>
               <p class="role">Front-End Developer</p>
             </li>
             <li>
-              <p class="name"><a href="http://twitter.com/ivonnekn">Audrey Kuo</a></p>
+              <p class="name"><a href="http://twitter.com/MartianMoose">Audrey Kuo</a></p>
               <p class="role">Front-End Developer</p>
             </li>
             <li>
@@ -99,7 +99,7 @@
 
     <section class="border page-content">
       <div class="wrapper-sm">
-        <h3>And finally, thank you to our readers and our community...</h3>
+        <h3>And finally, THANK YOU to our readers and our community...</h3>
           <p> for listening to our stories, sharing them and amplifying them!</p>
       </div> <!-- cls wrapper-sm -->
     </section>
